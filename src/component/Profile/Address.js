84432eb53b21e6575b0c0563c086a024d17bba79
core.pager=cat - <mikepool599@gmail.com>
2021-11-12 08:40:34 +0100

    first commit

---
+++ src/component/Profile/Address.js
@@ -0,0 +1,15 @@
+import React from "react" 
+
+
+
+function Address() {
+    return(
+        
+        <div className= "Add-container">
+        <h4>{"7a Milverton road"}</h4>
+        </div>
+    )        
+}
+
+export default Address
+
---
+++ src/component/Profile/FullName.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function FullName() {
+    return(
+
+        <div className="Fname">
+            <h3>{"Olaiya Micheal Boluwatife"}</h3>
+        </div>
+    )
+    
+};
+
+export default FullName
